5ed602f64cb4547eda06a7a509d0d25bb1372fd0
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-02-28 19:28:58 +0100

    Added G_DC.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_DC.vhd
@@ -0,0 +1,30 @@
+-- G_DC.vhd
+-- Generic D flip-flop with clear.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_DC is
+    Port
+    (
+      D : In std_logic;
+      CLK : In std_logic;
+      C : In std_logic;
+      Q : Out std_logic
+    );
+  end G_DC;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_DC is
+    begin
+      process (CLK, C)
+        begin
+          if C = '1' then
+            Q <= '0';
+          elsif rising_edge (CLK) then
+            Q <= D and D ;
+          end if;
+      end process;
+  end behavioral;
+
+-- EOF
