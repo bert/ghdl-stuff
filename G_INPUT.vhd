169a208dbb2e977e7b59ec3581f18193cf2c3528
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-02-28 08:01:27 +0100

    Added G_INPUT.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_INPUT.vhd
@@ -0,0 +1,22 @@
+-- G_INPUT.vhd
+-- Generic input port.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_INPUT is
+    Port
+    (
+      I : In std_logic;
+      O : Out std_logic
+    );
+  end G_INPUT;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_INPUT is
+    begin
+      O <= (I);
+    end behavioral;
+
+-- EOF
+
