003dc76d46b18fb51b75e6168ed4d0f6dc765a24
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-02-29 00:09:39 +0100

    Added G_2NAND.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_2NAND.vhd
@@ -0,0 +1,22 @@
+-- G_2NAND.vhd
+-- Generic 2 input NAND gate.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_2NAND is
+    Port
+    (
+      A : In std_logic;
+      B : In std_logic;
+      YN : Out std_logic
+    );
+  end G_2NAND;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_2NAND is
+    begin
+      YN <= not (A and B);
+    end behavioral;
+
+-- EOF
