8bd991bdb5be3163ea3e3c622f9943172598e2fc
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-02-28 15:57:22 +0100

    Added G_CLKBUF.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_CLKBUF.vhd
@@ -0,0 +1,22 @@
+-- G_CLKBUF.vhd
+-- Generic clock buffer.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_CLKBUF is
+    Port
+    (
+      I : In std_logic;
+      O : Out std_logic
+    );
+  end G_CLKBUF;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_CLKBUF is
+    begin
+      O <= (I);
+    end behavioral;
+
+-- EOF
+
