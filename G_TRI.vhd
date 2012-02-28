88b7343d57d8664c328b805b33d3aa7cc4d58571
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-02-28 15:56:49 +0100

    Added G_TRI.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_TRI.vhd
@@ -0,0 +1,23 @@
+-- G_TRI.vhd
+-- Generic tri-state buffer port.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_TRI is
+    Port
+    (
+      A0 : In std_logic;
+      OE : In std_logic;
+      XB0 : Out std_logic
+    );
+  end G_TRI;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_TRI is
+    begin
+      XB0 <= A0 when OE='1' else 'Z';
+    end behavioral;
+
+-- EOF
+
