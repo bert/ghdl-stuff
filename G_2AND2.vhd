c9fd5b2be9f03fc7b7e589e7b0269a02878ccc93
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-02-29 07:43:35 +0100

    Added G_2AND2.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_2AND2.vhd
@@ -0,0 +1,22 @@
+-- G_2AND2.vhd
+-- Generic AND gate with 2 inputs, 2 inputs inverted.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_2AND2 is
+    Port
+    (
+      AN : In std_logic;
+      BN : In std_logic;
+      Y : Out std_logic
+    );
+  end G_2AND2;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_2AND2 is
+    begin
+      Y <= not(AN) and not(BN);
+    end behavioral;
+
+-- EOF
