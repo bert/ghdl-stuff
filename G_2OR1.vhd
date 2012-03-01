bf9a4135e9be2eccc234289c492e552973288d5e
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-01 21:44:34 +0100

    Added G_2OR1.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_2OR1.vhd
@@ -0,0 +1,22 @@
+-- G_2OR1.vhd
+-- Generic 2 input OR gate, 1 input inverted.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_2OR1 is
+    Port
+    (
+      B : In std_logic;
+      AN : In std_logic;
+      Y : Out std_logic
+    );
+  end G_2OR1;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_2OR1 is
+    begin
+      Y <= not (AN) or B;
+    end behavioral;
+
+-- EOF
