94d72e17605b0099878496a20a56962c0c3f3752
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-09 07:59:08 +0100

    Added G_3NAND1.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_3NAND1.vhd
@@ -0,0 +1,23 @@
+-- G_3NAND1.vhd
+-- Generic 3 input NAND gate, 1 inverted input.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_3NAND1 is
+    Port
+    (
+      B : In std_logic;
+      C : In std_logic;
+      AN : In std_logic;
+      YN : Out std_logic
+    );
+  end G_3NAND1;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_3NAND1 is
+    begin
+      YN <= not (not (AN) and B and C);
+    end behavioral;
+
+-- EOF
