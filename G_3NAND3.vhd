db19a4cc9a002f171f7578273b2ac5691b904e09
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-11 16:18:11 +0100

    Added G_3NAND3.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_3NAND3.vhd
@@ -0,0 +1,23 @@
+-- G_3NAND3.vhd
+-- Generic 3 input NAND gate, 3 inputs inverted.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_3NAND3 is
+    Port
+    (
+      AN : In std_logic;
+      BN : In std_logic;
+      CN : In std_logic;
+      YN : Out std_logic
+    );
+  end G_3NAND3;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_3NAND3 is
+    begin
+      YN <= not (not (AN) and not (BN) and not (CN));
+    end behavioral;
+
+-- EOF
