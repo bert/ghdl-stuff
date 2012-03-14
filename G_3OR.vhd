d1f96b665220ea3c5c93a3bc3f48c68e3f0b890d
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-14 07:08:18 +0100

    Added G_3OR.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_3OR.vhd
@@ -0,0 +1,23 @@
+-- G_3OR.vhd
+-- Generic 3 input OR gate.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_3OR is
+    Port
+    (
+      A : In std_logic;
+      B : In std_logic;
+      C : In std_logic;
+      Y : Out std_logic
+    );
+  end G_3OR;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_3OR is
+    begin
+      Y <= A or B or C;
+    end behavioral;
+
+-- EOF
