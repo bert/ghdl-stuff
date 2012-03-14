167991ad04163e07b66eaa1a3fc3f0dfa11b138d
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-14 19:57:38 +0100

    Added G_3NOR.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_3NOR.vhd
@@ -0,0 +1,23 @@
+-- G_3NOR.vhd
+-- Generic 3 input NOR gate.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_3NOR is
+    Port
+    (
+      A : In std_logic;
+      B : In std_logic;
+      C : In std_logic;
+      YN : Out std_logic
+    );
+  end G_3NOR;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_3NOR is
+    begin
+      YN <= not (A or B or C);
+    end behavioral;
+
+-- EOF
