d4327b2ceee3ce764fec3d4e5fd33734cbb218b1
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-17 14:57:52 +0100

    Added G_XOR.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_XOR.vhd
@@ -0,0 +1,22 @@
+-- G_XOR.vhd
+-- Generic exclusive OR gate.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_XOR is
+    Port
+    (
+      A : In std_logic;
+      B : In std_logic;
+      Y : Out std_logic
+    );
+  end G_XOR;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_XOR is
+    begin
+      Y <= A xor B;
+    end behavioral;
+
+-- EOF
