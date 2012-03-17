b1d3b3bc8b2756cba71b101f86c26acfded1a140
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-17 14:58:13 +0100

    Added G_XNOR.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_XNOR.vhd
@@ -0,0 +1,22 @@
+-- G_XNOR.vhd
+-- Generic Exclusive NOR gate.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_XNOR is
+    Port
+    (
+      A : In std_logic;
+      B : In std_logic;
+      YN : Out std_logic
+    );
+  end G_XNOR;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_XNOR is
+    begin
+      YN <= not (A xor B);
+    end behavioral;
+
+-- EOF
