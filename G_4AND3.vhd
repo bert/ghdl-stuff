3debd41c160ffec691129b2db0bfa77000c9c1fb
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-22 22:57:16 +0100

    Added G_4AND3.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_4AND3.vhd
@@ -0,0 +1,24 @@
+-- G_4AND3.vhd
+-- Generic 4 input AND gate, 3 inverted inputs.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_4AND3 is
+    Port
+    (
+      D : In std_logic;
+      AN : In std_logic;
+      BN : In std_logic;
+      CN : In std_logic;
+      Y : Out std_logic
+    );
+  end G_4AND3;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_4AND3 is
+    begin
+      Y <= not (AN) and not (BN) and not (CN) and D;
+    end behavioral;
+
+-- EOF
