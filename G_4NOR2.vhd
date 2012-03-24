a532adf152746ec7fe04c378a05ee7ddf9597ab8
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-24 16:26:18 +0100

    Added G_4NOR2.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_4NOR2.vhd
@@ -0,0 +1,24 @@
+-- G_4NOR2.vhd
+-- Generic 4 input NOR gate, 2 inputs inverted.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_4NOR2 is
+    Port
+    (
+      C : In std_logic;
+      D : In std_logic;
+      AN : In std_logic;
+      BN : In std_logic;
+      YN : Out std_logic
+    );
+  end G_4NOR2;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_4NOR2 is
+    begin
+      YN <= not (not (AN) or not (BN) or C or D);
+    end behavioral;
+
+-- EOF
