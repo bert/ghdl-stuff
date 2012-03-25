e701b642bfea08feb87e3bddabc5ba8e0daef2b8
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-25 14:26:24 +0200

    Added G_4OR2.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_4OR2.vhd
@@ -0,0 +1,24 @@
+-- G_4OR2.vhd
+-- Generic 4 input OR gate, 2 inputs inverted.
+
+library ieee;
+use ieee.std_logic_1164.all;
+  entity G_4OR2 is
+    Port
+    (
+      C : In std_logic;
+      D : In std_logic;
+      AN : In std_logic;
+      BN : In std_logic;
+      Y : Out std_logic
+    );
+  end G_4OR2;
+
+library ieee;
+use ieee.std_logic_1164.all;
+  architecture behavioral of G_4OR2 is
+    begin
+      Y <= not (AN) or not (BN) or C or D;
+    end behavioral;
+
+-- EOF
