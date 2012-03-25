ef0af679e0bdb41fc32998213b7869b06f355146
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-25 15:03:22 +0200

    Added G_MUX21.vhd.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ G_MUX21.vhd
@@ -0,0 +1,24 @@
+-- G_MUX21.vhd
+-- Generic 2 to 1 multiplexer.
+
+library ieee;
+use ieee.std_logic_1164.all;
+entity G_MUX21 is
+  port
+  (
+    A : In std_logic;
+    B : In std_logic;
+    S : In std_logic;
+    Y : Out std_logic
+  );
+end G_MUX21;
+
+library ieee;
+use ieee.std_logic_1164.all;
+architecture behavioral of G_MUX21 is
+  begin
+    Y <= (A and (not S)) or (B and S) ; 
+  end behavioral;
+
+
+-- EOF
