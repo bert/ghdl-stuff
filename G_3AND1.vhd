008ee752c82bf43db6e1f96be3fb4459acc1cd60
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-31 23:05:42 +0200

    G_3AND1.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_3AND1.vhd
+++ G_3AND1.vhd
@@ -3,21 +3,21 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_3AND1 is
-    Port
-    (
-      B : In std_logic;
-      C : In std_logic;
-      AN : In std_logic;
-      Y : Out std_logic
-    );
-  end G_3AND1;
+entity G_3AND1 is
+  port
+  (
+    B : In std_logic;
+    C : In std_logic;
+    AN : In std_logic;
+    Y : Out std_logic
+  );
+end G_3AND1;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_3AND1 is
-    begin
-      Y <= not (AN) and B and C;
-    end behavioral;
+architecture behavioral of G_3AND1 is
+begin
+  Y <= not (AN) and B and C;
+end behavioral;
 
 -- EOF
