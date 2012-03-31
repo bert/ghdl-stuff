539c01beeaccabe5c470480f7f44ebd9e8ef1e98
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-31 22:20:45 +0200

    G_2AND1.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_2AND1.vhd
+++ G_2AND1.vhd
@@ -3,20 +3,20 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_2AND1 is
-    Port
-    (
-      B : In std_logic;
-      AN : In std_logic;
-      Y : Out std_logic
-    );
-  end G_2AND1;
+entity G_2AND1 is
+  port
+  (
+    B : In std_logic;
+    AN : In std_logic;
+    Y : Out std_logic
+  );
+end G_2AND1;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_2AND1 is
-    begin
-      Y <= not (AN) and B;
-    end behavioral;
+architecture behavioral of G_2AND1 is
+begin
+  Y <= not (AN) and B;
+end behavioral;
 
 -- EOF
