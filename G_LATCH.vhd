4c06b7737ab9f50f5b8e8464873c4989d101fc44
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-03-31 18:11:46 +0200

    G_LATCH.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_LATCH.vhd
+++ G_LATCH.vhd
@@ -3,25 +3,25 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_LATCH is
-    Port
-    (
-      D : In std_logic;
-      E : In std_logic;
-      Q : Out std_logic
-    );
-  end G_LATCH;
+entity G_LATCH is
+  port
+  (
+    D : In std_logic;
+    E : In std_logic;
+    Q : Out std_logic
+  );
+end G_LATCH;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_LATCH is
-    begin
-      process (E, D)
-        begin
-          if E = '1' then
-            Q <= D;
-          end if;
-      end process;
-  end behavioral;
+architecture behavioral of G_LATCH is
+begin
+  process (E, D)
+  begin
+    if E = '1' then
+      Q <= D;
+    end if;
+  end process;
+end behavioral;
 
 -- EOF
