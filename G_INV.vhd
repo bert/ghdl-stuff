df81e3086a8af0f03f2c79c20380136d5df55b26
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 20:37:21 +0200

    G_INV.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_INV.vhd
+++ G_INV.vhd
@@ -3,20 +3,20 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_INV is
-    Port
-    (
-      A : In std_logic;
-      YN : Out std_logic
-    );
-  end G_INV;
+entity G_INV is
+  port
+  (
+    A : In std_logic;
+    YN : Out std_logic
+  );
+end G_INV;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_INV is
-    begin
-      YN <= not (A);
-    end behavioral;
+architecture behavioral of G_INV is
+begin
+  YN <= not (A);
+end behavioral;
 
 -- EOF
 
