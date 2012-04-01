81b22b35daee71709a7ec22c06824b6faec0e1ab
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 16:26:34 +0200

    G_4NOR.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_4NOR.vhd
+++ G_4NOR.vhd
@@ -2,22 +2,22 @@
 -- Generic 4 input NOR gate.
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_4NOR is
-    Port
-    (
-      A : In std_logic;
-      B : In std_logic;
-      C : In std_logic;
-      D : In std_logic;
-      YN : Out std_logic
-    );
-  end G_4NOR;
+entity G_4NOR is
+  port
+  (
+    A : In std_logic;
+    B : In std_logic;
+    C : In std_logic;
+    D : In std_logic;
+    YN : Out std_logic
+  );
+end G_4NOR;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_4NOR is
-    begin
-      YN <= not (A or B or C or D);
-    end behavioral;
+architecture behavioral of G_4NOR is
+begin
+  YN <= not (A or B or C or D);
+end behavioral;
 
 -- EOF
