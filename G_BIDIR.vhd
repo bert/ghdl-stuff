3ecab9667d3bd79f5502bc120c089a238b654c58
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 16:35:57 +0200

    G_BIDIR.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_BIDIR.vhd
+++ G_BIDIR.vhd
@@ -3,24 +3,24 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_BIDIR is
-    Port
-    (
-      A0 : In std_logic;
-      OE : In std_logic;
-      XB0 : InOut std_logic;
-      Z0 : Out std_logic
-    );
-  end G_BIDIR;
+entity G_BIDIR is
+  port
+  (
+    A0 : In std_logic;
+    OE : In std_logic;
+    XB0 : InOut std_logic;
+    Z0 : Out std_logic
+  );
+end G_BIDIR;
 
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_BIDIR is
-    begin
-      XB0 <= A0 when OE='1' else 'Z';
-      Z0 <= XB0;
-    end behavioral;
+architecture behavioral of G_BIDIR is
+begin
+  XB0 <= A0 when OE='1' else 'Z';
+  Z0 <= XB0;
+end behavioral;
 
 -- EOF
 
