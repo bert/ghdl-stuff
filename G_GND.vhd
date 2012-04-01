7a4afb4b00f0ddbcdb837ecd9ce270d192d10ca6
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 20:23:54 +0200

    G_GND.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_GND.vhd
+++ G_GND.vhd
@@ -3,16 +3,16 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_GND is
-    port
-    (
-      Y : Out std_logic
-    );
-  end;
+entity G_GND is
+  port
+  (
+    Y : Out std_logic
+  );
+end;
 
-  architecture behavioral of G_GND is
-    begin
-      Y <= '0';
-    end behavioral;
+architecture behavioral of G_GND is
+begin
+  Y <= '0';
+end behavioral;
 
 -- EOF
