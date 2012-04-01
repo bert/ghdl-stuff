a44eae91a44ab4440a8898661f46413b85b27778
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 16:31:45 +0200

    G_4OR1.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_4OR1.vhd
+++ G_4OR1.vhd
@@ -3,22 +3,22 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_4OR1 is
-    Port
-    (
-      B : In std_logic;
-      C : In std_logic;
-      D : In std_logic;
-      AN : In std_logic;
-      Y : Out std_logic
-    );
-  end G_4OR1;
+entity G_4OR1 is
+  port
+  (
+    B : In std_logic;
+    C : In std_logic;
+    D : In std_logic;
+    AN : In std_logic;
+    Y : Out std_logic
+  );
+end G_4OR1;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_4OR1 is
-    begin
-      Y <= not (AN) or B or C or D;
-    end behavioral;
+architecture behavioral of G_4OR1 is
+begin
+  Y <= not (AN) or B or C or D;
+end behavioral;
 
 -- EOF
