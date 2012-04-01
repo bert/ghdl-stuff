45fc9629a604245194f9c8a5cbafcc24be7c6f3c
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 08:55:48 +0200

    G_3NOR2.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_3NOR2.vhd
+++ G_3NOR2.vhd
@@ -3,21 +3,21 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_3NOR2 is
-    Port
-    (
-      C : In std_logic;
-      AN : In std_logic;
-      BN : In std_logic;
-      YN : Out std_logic
-    );
-  end G_3NOR2;
+entity G_3NOR2 is
+  port
+  (
+    C : In std_logic;
+    AN : In std_logic;
+    BN : In std_logic;
+    YN : Out std_logic
+  );
+end G_3NOR2;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_3NOR2 is
-    begin
-      YN <= not (not (AN) or not (BN) or C);
-    end behavioral;
+architecture behavioral of G_3NOR2 is
+begin
+  YN <= not (not (AN) or not (BN) or C);
+end behavioral;
 
 -- EOF
