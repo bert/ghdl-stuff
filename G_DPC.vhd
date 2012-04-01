6c34fbdde3b415109a657c0422035c23750fcfd9
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 20:08:53 +0200

    G_DPC.vhd: changed description and indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_DPC.vhd
+++ G_DPC.vhd
@@ -1,33 +1,33 @@
 -- G_DPC.vhd
--- Generic D flip-flop with clear.
+-- Generic D flip-flop with preset and clear.
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_DPC is
-    Port
-    (
-      D : In std_logic;
-      P : In std_logic;
-      CLK : In std_logic;
-      C : In std_logic;
-      Q : Out std_logic
-    );
-  end G_DPC;
+entity G_DPC is
+  port
+  (
+    D : In std_logic;
+    P : In std_logic;
+    CLK : In std_logic;
+    C : In std_logic;
+    Q : Out std_logic
+  );
+end G_DPC;
 
 library ieee;
 use ieee.std_logic_1164.all;
 architecture behavioral of G_DPC is
+begin
+  process (CLK, C, P)
   begin
-    process (CLK, C, P)
-      begin
-        if C = '1' then
-          Q <= '0';
-        elsif P = '1' then
-          Q <= '1';
-        elsif rising_edge (CLK) then
-          Q <= D AND D;
-        end if;
-      end process;
-  end behavioral;
+    if C = '1' then
+      Q <= '0';
+    elsif P = '1' then
+      Q <= '1';
+    elsif rising_edge (CLK) then
+      Q <= D AND D;
+    end if;
+  end process;
+end behavioral;
 
 -- EOF
