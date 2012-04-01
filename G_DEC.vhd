543342b2f2da3dbdd9dff6cc6513e4de77c9c10b
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 16:47:29 +0200

    G_DEC.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_DEC.vhd
+++ G_DEC.vhd
@@ -3,30 +3,30 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_DEC is
-    Port
-    (
-      D : In std_logic;
-      E : In std_logic;
-      C : In std_logic;
-      CLK : In std_logic;
-      Q : Out std_logic
-    );
-  end G_DEC;
+entity G_DEC is
+  port
+  (
+    D : In std_logic;
+    E : In std_logic;
+    CLK : In std_logic;
+    C : In std_logic;
+    Q : Out std_logic
+  );
+end G_DEC;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_DEC is
-    begin
-      process(CLK, C, E)
-        begin
-          if C = '1' then
-            Q <= '0';
-          elsif rising_edge (CLK) then
-            if E = '1' then
-              Q <= D and D;
-            end if;
-          end if;
-      end process;
-    end behavioral;
+architecture behavioral of G_DEC is
+begin
+  process (CLK, C, E)
+  begin
+    if C = '1' then
+      Q <= '0';
+    elsif rising_edge (CLK) then
+      if E = '1' then
+        Q <= D and D;
+      end if;
+    end if;
+  end process;
+end behavioral;
 
