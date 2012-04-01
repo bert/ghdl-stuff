6b0475a3a7d25c8d99639ee1c34c503339ab1893
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 20:38:11 +0200

    G_HADD.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_HADD.vhd
+++ G_HADD.vhd
@@ -11,15 +11,15 @@ entity G_HADD is
     SUM : out std_logic;
     CO: out std_logic
   );
-  end G_HADD;
+end G_HADD;
 
 library ieee;
 use ieee.std_logic_1164.all;
 architecture behavioral of G_HADD is
-  signal s: std_logic;
-    begin 
-      SUM <= A xor B;
-      CO <= A and B;
-    end behavioral;
+signal s: std_logic;
+begin 
+  SUM <= A xor B;
+  CO <= A and B;
+end behavioral;
 
 -- EOF
