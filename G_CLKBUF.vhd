d818cf59e0064a6b03619f93d5386a2c04e2602c
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 16:37:10 +0200

    G_CLKBUF.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_CLKBUF.vhd
+++ G_CLKBUF.vhd
@@ -3,20 +3,20 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_CLKBUF is
-    Port
-    (
-      I : In std_logic;
-      O : Out std_logic
-    );
-  end G_CLKBUF;
+entity G_CLKBUF is
+  port
+  (
+    I : In std_logic;
+    O : Out std_logic
+  );
+end G_CLKBUF;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_CLKBUF is
-    begin
-      O <= (I);
-    end behavioral;
+architecture behavioral of G_CLKBUF is
+begin
+  O <= (I);
+end behavioral;
 
 -- EOF
 
