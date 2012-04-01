768875bb6ad095172e82d70e33567e4090b6a21a
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 20:37:47 +0200

    G_FADD.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_FADD.vhd
+++ G_FADD.vhd
@@ -12,16 +12,16 @@ entity G_FADD is
     SUM out std_logic;
     CO: out std_logic
   );
-  end G_FADD;
+end G_FADD;
 
 library ieee;
 use ieee.std_logic_1164.all;
 architecture behavioral of G_FADD is
-  signal s: std_logic;
-    begin 
-      s <= a xor b;
-      SUM <= s xor CI;
-      CO <= (A and B) or (s and CI);
-    end behavioral;
+signal s: std_logic;
+begin 
+  s <= a xor b;
+  SUM <= s xor CI;
+  CO <= (A and B) or (s and CI);
+end behavioral;
 
 -- EOF
