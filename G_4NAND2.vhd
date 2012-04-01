854c428ddb0e76f3b21e2b8fd916c6ccfe02b27a
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 16:23:32 +0200

    G_4NAND2.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_4NAND2.vhd
+++ G_4NAND2.vhd
@@ -3,22 +3,22 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_4NAND2 is
-    Port
-    (
-      C : In std_logic;
-      D : In std_logic;
-      AN : In std_logic;
-      BN : In std_logic;
-      YN : Out std_logic
-    );
-  end G_4NAND2;
+entity G_4NAND2 is
+  port
+  (
+    C : In std_logic;
+    D : In std_logic;
+    AN : In std_logic;
+    BN : In std_logic;
+    YN : Out std_logic
+  );
+end G_4NAND2;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_4NAND2 is
-    begin
-      YN <= not (not (AN) and not (BN) and C and D);
-    end behavioral;
+architecture behavioral of G_4NAND2 is
+begin
+  YN <= not (not (AN) and not (BN) and C and D);
+end behavioral;
 
 -- EOF
