f5687b54314b8ff1dfd6f5fb38405b970bd91b6c
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 16:27:28 +0200

    G_4NOR3.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_4NOR3.vhd
+++ G_4NOR3.vhd
@@ -3,22 +3,22 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_4NOR3 is
-    Port
-    (
-      D : In std_logic;
-      AN : In std_logic;
-      BN : In std_logic;
-      CN : In std_logic;
-      YN : Out std_logic
-    );
-  end G_4NOR3;
+entity G_4NOR3 is
+  port
+  (
+    D : In std_logic;
+    AN : In std_logic;
+    BN : In std_logic;
+    CN : In std_logic;
+    YN : Out std_logic
+  );
+end G_4NOR3;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_4NOR3 is
-    begin
-      YN <= not (not (AN) or not (BN) or not (CN) or D);
-    end behavioral;
+architecture behavioral of G_4NOR3 is
+begin
+  YN <= not (not (AN) or not (BN) or not (CN) or D);
+end behavioral;
 
 -- EOF
