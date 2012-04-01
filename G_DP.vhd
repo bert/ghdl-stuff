d2a1f5174b632b33289ea50c41969ad70d9af6b2
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-01 20:08:11 +0200

    G_DP.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_DP.vhd
+++ G_DP.vhd
@@ -3,28 +3,28 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_DP is
-    Port
-    (
-      D : In std_logic;
-      CLK : In std_logic;
-      P : In std_logic;
-      Q : Out std_logic
-    );
-  end G_DP;
+entity G_DP is
+  port
+  (
+    D : In std_logic;
+    CLK : In std_logic;
+    P : In std_logic;
+    Q : Out std_logic
+  );
+end G_DP;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_DP is
-    begin
-      process (CLK, P)
-        begin
-          if P = '1' then
-            Q <= '1';
-          elsif rising_edge (CLK) then
-            Q <= D and D;
-        end if;
-    end process;
-  end behavioral;
+architecture behavioral of G_DP is
+begin
+  process (CLK, P)
+  begin
+    if P = '1' then
+      Q <= '1';
+    elsif rising_edge (CLK) then
+      Q <= D and D;
+    end if;
+  end process;
+end behavioral;
 
 -- EOF
