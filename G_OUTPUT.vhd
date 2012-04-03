b1a2c39b1181cf60c938101805b43abb92047776
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-03 07:43:25 +0200

    G_OUTPUT.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_OUTPUT.vhd
+++ G_OUTPUT.vhd
@@ -3,20 +3,20 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_OUTPUT is
-    Port
-    (
-      I : In std_logic;
-      O : Out std_logic
-    );
-  end G_OUTPUT;
+entity G_OUTPUT is
+  port
+  (
+    I : In std_logic;
+    O : Out std_logic
+  );
+end G_OUTPUT;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_OUTPUT is
-    begin
-      O <= (I);
-    end behavioral;
+architecture behavioral of G_OUTPUT is
+begin
+  O <= (I);
+end behavioral;
 
 -- EOF
 
