292e9a970586237330507a8b519cc2a0f0da0b0f
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-07 01:12:07 +0200

    G_XNOR.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_XNOR.vhd
+++ G_XNOR.vhd
@@ -3,20 +3,20 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_XNOR is
-    Port
-    (
-      A : In std_logic;
-      B : In std_logic;
-      YN : Out std_logic
-    );
-  end G_XNOR;
+entity G_XNOR is
+  port
+  (
+    A : In std_logic;
+    B : In std_logic;
+    YN : Out std_logic
+  );
+end G_XNOR;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_XNOR is
-    begin
-      YN <= not (A xor B);
-    end behavioral;
+architecture behavioral of G_XNOR is
+begin
+  YN <= not (A xor B);
+end behavioral;
 
 -- EOF
