27623391740bd7016e75e7fc1e2e4cbfaea10f51
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-07 01:12:29 +0200

    G_XOR.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_XOR.vhd
+++ G_XOR.vhd
@@ -3,20 +3,20 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_XOR is
-    Port
-    (
-      A : In std_logic;
-      B : In std_logic;
-      Y : Out std_logic
-    );
-  end G_XOR;
+entity G_XOR is
+  port
+  (
+    A : In std_logic;
+    B : In std_logic;
+    Y : Out std_logic
+  );
+end G_XOR;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_XOR is
-    begin
-      Y <= A xor B;
-    end behavioral;
+architecture behavioral of G_XOR is
+begin
+  Y <= A xor B;
+end behavioral;
 
 -- EOF
