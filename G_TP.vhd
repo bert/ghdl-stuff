08fd2c882dbef0dc2653da54a411a6b6a8bb038e
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-07 01:10:43 +0200

    G_TP.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_TP.vhd
+++ G_TP.vhd
@@ -3,33 +3,32 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_TP is
-    Port
-    (
-      T : In std_logic;
-      CLK : In std_logic;
-      P : In std_logic;
-      Q : Out std_logic
-    );
-  end G_TP;
+entity G_TP is
+  port
+  (
+    T : In std_logic;
+    CLK : In std_logic;
+    P : In std_logic;
+    Q : Out std_logic
+  );
+end G_TP;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_TP is
+architecture behavioral of G_TP is
+begin
+  process (CLK, P)
+  variable Q_local: std_logic;
   begin
-    process (CLK, P)
-    variable Q_local: std_logic;
-
-    begin
-      if P = '1' then
-        Q_local := '1';
-      elsif rising_edge (CLK) then
-        if T = '1' then
-          Q_local := not Q_local;
-        end if;
+    if P = '1' then
+      Q_local := '1';
+    elsif rising_edge (CLK) then
+      if T = '1' then
+        Q_local := not Q_local;
       end if;
-      Q <= Q_local;
-    end process;
-  end behavioral;
+    end if;
+    Q <= Q_local;
+  end process;
+end behavioral;
 
 -- EOF
