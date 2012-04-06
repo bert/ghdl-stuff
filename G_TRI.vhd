119fde8e5503dbc6bb7ac58b8e922dd6e5c0ac54
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-07 01:11:14 +0200

    G_TRI.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_TRI.vhd
+++ G_TRI.vhd
@@ -3,21 +3,21 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_TRI is
-    Port
-    (
-      A0 : In std_logic;
-      OE : In std_logic;
-      XB0 : Out std_logic
-    );
-  end G_TRI;
+entity G_TRI is
+  port
+  (
+    A0 : In std_logic;
+    OE : In std_logic;
+    XB0 : Out std_logic
+  );
+end G_TRI;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_TRI is
-    begin
-      XB0 <= A0 when OE='1' else 'Z';
-    end behavioral;
+architecture behavioral of G_TRI is
+begin
+  XB0 <= A0 when OE='1' else 'Z';
+end behavioral;
 
 -- EOF
 
