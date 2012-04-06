f4c8a56dc20ab160a6ba3bbbbf8adc3e09aec807
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-04-07 01:10:02 +0200

    G_TC.vhd: changed indentation.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- G_TC.vhd
+++ G_TC.vhd
@@ -2,32 +2,32 @@
 -- Generic T flip-flop with clear.
 library ieee;
 use ieee.std_logic_1164.all;
-  entity G_TC is
-    Port
-    (
-      T : In std_logic;
-      CLK : In std_logic;
-      C : In std_logic;
-      Q : Out std_logic
-    );
-  end G_TC;
+entity G_TC is
+  port
+  (
+    T : In std_logic;
+    CLK : In std_logic;
+    C : In std_logic;
+    Q : Out std_logic
+  );
+end G_TC;
 
 library ieee;
 use ieee.std_logic_1164.all;
-  architecture behavioral of G_TC is
-    begin
-      process (CLK, C)
-      variable Q_local: std_logic;
-      begin
-        if C = '1' then
-          Q_local := '0';
-        elsif rising_edge (CLK) then
-          if T = '1' then
-            Q_local := not Q_local;
-          end if;
-        end if;
-        Q <= Q_local;
-      end process;
-    end behavioral;
+architecture behavioral of G_TC is
+begin
+  process (CLK, C)
+  variable Q_local: std_logic;
+  begin
+    if C = '1' then
+      Q_local := '0';
+    elsif rising_edge (CLK) then
+      if T = '1' then
+        Q_local := not Q_local;
+      end if;
+    end if;
+    Q <= Q_local;
+  end process;
+end behavioral;
 
 -- EOF
